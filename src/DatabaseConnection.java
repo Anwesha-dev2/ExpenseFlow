11d008ff48efe7a182f0aa9a8e2c4c83e3fca8c7
Anwesha Bastia <anweshabastia56@gmail.com>
2026-09-23 22:49:07 +0530

    Create DatabaseConnection.java

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/expense_management";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "YOUR_MYSQL_PASSWORD";
+
+    public static Connection getConnection() {
+
+        try {
+            Connection connection =
+                    DriverManager.getConnection(URL, USER, PASSWORD);
+
+            System.out.println("Database connected successfully!");
+
+            return connection;
+
+        } catch (SQLException e) {
+
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+}
